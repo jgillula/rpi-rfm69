afed672bc29ef3e3bc7ff888190bd20f7bb08bc5
Jeremy Gillula <jgillula+github@gmail.com>
2021-09-15 09:11:49 -0700

    Hopefully a shiny new pylint badge

--- .github/workflows/run_pylint.py
+++ .github/workflows/run_pylint.py
@@ -1,17 +1,25 @@
 import sys
+import math
 from pylint.lint import Run
-import anybadge
+#import anybadge
 
-# Define thresholds: <2=red, <4=orange <8=yellow <10=green
-thresholds = {2: 'red',
-              4: 'orange',
-              6: 'yellow',
-              10: 'green'}
+# Define thresholds: <3=red, <6=orange <9=yellow <10=green =10=brightgreen
+thresholds = {3: 'red',
+              6: 'orange',
+              9: 'yellow',
+              10: 'green',
+              math.inf: 'brightgreen'}
 
 results = Run(['--disable=import-error,unused-wildcard-import,wildcard-import', 'RFM69'], do_exit=False)
 # `exit` is deprecated, use `do_exit` instead
-badge = anybadge.Badge('pylint', round(results.linter.stats['global_note'], 2), thresholds=thresholds)
-badge.write_badge('pylint.svg')
+#badge = anybadge.Badge('pylint', round(results.linter.stats['global_note'], 2), thresholds=thresholds)
+#badge.write_badge('pylint.svg')
+rating = results.linter.stats['global_note']
+print("##[set-output name=rating]{:.2f}".format(rating))
+for value in thresholds.keys():
+    if rating < value:
+        print("##[set-output name=color]{}".format(thresholds[value]))
+        break
 sys.exit(min(1, results.linter.stats["fatal"]
              + results.linter.stats["error"]
              + results.linter.stats["warning"]))
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-[![Documentation Status](https://readthedocs.org/projects/rpi-rfm69/badge/?version=latest)](https://rpi-rfm69.readthedocs.io/en/latest/?badge=latest) [![pylint](https://github.com/jgillula/rpi-rfm69/actions/workflows/pylint.yml/pylint.svg)](https://github.com/jgillula/rpi-rfm69/actions/workflows/pylint.yml)
+[![Documentation Status](https://readthedocs.org/projects/rpi-rfm69/badge/?version=latest)](https://rpi-rfm69.readthedocs.io/en/latest/?badge=latest)
+![badge](https://img.shields.io/endpoint?url=https://gist.githubusercontent.com/jgillula/385e5dc0d1b4f63dffa3de2db8695a69/raw/test.json)
+
 
 # RFM69 Radio interface for the Raspberry Pi
 This package provides a Python wrapper of the [LowPowerLabs RFM69 library](https://github.com/LowPowerLab/RFM69) and is largely based on the work of [Eric Trombly](https://github.com/etrombly/RFM69) who ported the library from C.
