29052e382965abd7a20d61fc00091cd91f2746ee
Jeremy Gillula <jgillula+github@gmail.com>
2021-09-18 14:28:34 -0700

    Fixing pylint

--- .github/workflows/run_pylint.py
+++ .github/workflows/run_pylint.py
@@ -1,5 +1,6 @@
 import sys
 import math
+from glob import glob
 from pylint.lint import Run
 
 # Define thresholds: <3=red, <6=orange <9=yellow <10=green =10=brightgreen
@@ -9,7 +10,7 @@ thresholds = {3: 'red',
               10: 'green',
               math.inf: 'brightgreen'}
 
-results = Run(['--disable=import-error,unused-wildcard-import,wildcard-import,line-too-long,invalid-name,missing-module-docstring,too-many-lines,too-many-instance-attributes,consider-using-f-string,too-many-locals,too-few-public-methods,too-many-branches,duplicate-code', 'RFM69', 'tests/*.py'], do_exit=False)
+results = Run(['--disable=import-error,unused-wildcard-import,wildcard-import,line-too-long,invalid-name,missing-module-docstring,too-many-lines,too-many-instance-attributes,consider-using-f-string,too-many-locals,too-few-public-methods,too-many-branches,duplicate-code', 'RFM69'] + glob("tests/*.py"), do_exit=False)
 rating = results.linter.stats['global_note']
 print("##[set-output name=rating]{:.2f}".format(rating))
 for value in thresholds.keys():
