643f1251c020a419a1debe59f627dc84dc8afd45
Jeremy Gillula <jgillula+github@gmail.com>
2021-09-18 17:39:19 -0700

    Bumping the pylint color thresholds down. I think I earned a bright green

--- .github/workflows/run_pylint.py
+++ .github/workflows/run_pylint.py
@@ -1,14 +1,13 @@
 import sys
-import math
 from glob import glob
 from pylint.lint import Run
 
-# Define thresholds: <3=red, <6=orange <9=yellow <10=green =10=brightgreen
+# Define thresholds: <3=red, <6=orange <8=yellow <9.5=green <10=brightgreen
 thresholds = {3: 'red',
               6: 'orange',
-              9: 'yellow',
-              10: 'green',
-              math.inf: 'brightgreen'}
+              8: 'yellow',
+              9.5: 'green',
+              10: 'brightgreen'}
 
 results = Run(['--disable=import-error,unused-wildcard-import,wildcard-import,line-too-long,invalid-name,missing-module-docstring,too-many-lines,too-many-instance-attributes,consider-using-f-string,too-many-locals,too-few-public-methods,too-many-branches,duplicate-code', 'RFM69'] + glob("tests/*.py") + glob("examples/*.py"), do_exit=False)
 
@@ -20,7 +19,7 @@ else:
     rating = results.linter.stats['global_note']
     print("##[set-output name=rating]{:.2f}".format(rating))
     for value in thresholds.keys():
-        if rating < value:
+        if rating <= value:
             print("##[set-output name=color]{}".format(thresholds[value]))
             break
     print("##[set-output name=linting_status]passed")
