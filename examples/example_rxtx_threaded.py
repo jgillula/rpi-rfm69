eba9caa60a6f89dfc864adcbc59ed17e4c993a5c
Jeremy Gillula <jgillula+github@gmail.com>
2021-09-24 15:41:03 -0700

    Cosmetic formatting changes to the example code

--- examples/example_rxtx_threaded.py
+++ examples/example_rxtx_threaded.py
@@ -17,9 +17,11 @@ def receiveFunction(radio):
         # Process packet
         print(packet)
 
-# The following are for an Adafruit RFM69HCW Transceiver Radio Bonnet https://www.adafruit.com/product/4072
+# The following are for an Adafruit RFM69HCW Transceiver Radio
+# Bonnet https://www.adafruit.com/product/4072
 # You should adjust them to whatever matches your radio
-with Radio(FREQ_915MHZ, node_id, network_id, isHighPower=True, verbose=False, interruptPin=15, resetPin=22, spiDevice=1) as radio:
+with Radio(FREQ_915MHZ, node_id, network_id, isHighPower=True, verbose=False,
+           interruptPin=15, resetPin=22, spiDevice=1) as radio:
     print ("Starting loop...")
 
     # Create a thread to run receiveFunction in the background and start it
