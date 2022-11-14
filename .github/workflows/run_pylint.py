51e84c66119ab6233b3114d2635b49b4a36c4ace
Jeremy Gillula <jgillula+github@gmail.com>
2022-11-13 16:30:01 -0800

    Fixing pylint again

--- .github/workflows/run_pylint.py
+++ .github/workflows/run_pylint.py
@@ -1,7 +1,13 @@
+import os
 import sys
 from glob import glob
 from pylint.lint import Run
 import pylint
+
+def save_env_var(env_var, value):
+    env_filename = os.getenv('GITHUB_ENV')
+    with open(env_filename, "a") as env_file:
+        env_file.write("{}={}".format(env_var, value))
 
 print("Running pylint version {}".format(pylint.__version__))
 
@@ -15,15 +21,15 @@ thresholds = {3: 'red',
 results = Run(['--disable=import-error,unused-wildcard-import,wildcard-import,line-too-long,invalid-name,missing-module-docstring,too-many-lines,too-many-instance-attributes,consider-using-f-string,too-many-locals,too-few-public-methods,too-many-branches,duplicate-code,too-many-public-methods', 'RFM69'] + glob("tests/*.py") + glob("examples/*.py"), exit=False)
 
 if results.linter.stats.fatal + results.linter.stats.error + results.linter.stats.warning > 0:
-    print("rating=failing!")
-    print("color=red")
-    print("linting_status=failed")
+    save_env_var("rating", "failing!")
+    save_env_var("color", "red")
+    save_env_var("linting_status", "failed")
 else:
     rating = results.linter.stats.global_note
-    print("rating={:.2f}".format(rating))
+    save_env_var("rating", "{:.2f}".format(rating))
     for value in thresholds.keys():
         if rating <= value:
-            print("color={}".format(thresholds[value]))
+            save_env_var("color", thresholds[value])
             break
-    print("linting_status=passed")
+    save_env_var("linting_status", "passed")
 sys.exit(0)
