1d217a5413eb7e760d216532c54bd03ddfbc1c53
Jeremy Gillula <jgillula+github@gmail.com>
2022-11-13 16:24:45 -0800

    Fixing pylint again

--- .github/workflows/run_pylint.py
+++ .github/workflows/run_pylint.py
@@ -15,15 +15,15 @@ thresholds = {3: 'red',
 results = Run(['--disable=import-error,unused-wildcard-import,wildcard-import,line-too-long,invalid-name,missing-module-docstring,too-many-lines,too-many-instance-attributes,consider-using-f-string,too-many-locals,too-few-public-methods,too-many-branches,duplicate-code,too-many-public-methods', 'RFM69'] + glob("tests/*.py") + glob("examples/*.py"), exit=False)
 
 if results.linter.stats.fatal + results.linter.stats.error + results.linter.stats.warning > 0:
-    print("##[set-output name=rating]failing!")
-    print("##[set-output name=color]red")
-    print("##[set-output name=linting_status]failed")
+    print("rating=failing!")
+    print("color=red")
+    print("linting_status=failed")
 else:
     rating = results.linter.stats.global_note
-    print("##[set-output name=rating]{:.2f}".format(rating))
+    print("rating={:.2f}".format(rating))
     for value in thresholds.keys():
         if rating <= value:
-            print("##[set-output name=color]{}".format(thresholds[value]))
+            print("color={}".format(thresholds[value]))
             break
-    print("##[set-output name=linting_status]passed")
+    print("linting_status=passed")
 sys.exit(0)
